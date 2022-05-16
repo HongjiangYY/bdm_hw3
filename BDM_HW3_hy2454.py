f8d48ff5036f2d701a1a82d8849aa6f34ecb15cf
HongjiangYY <hongjiangye@gmail.com>
2022-05-15 22:52:21 -0400

    Add files via upload

--- BDM_HW3_hy2454.py
+++ BDM_HW3_hy2454.py
@@ -8,12 +8,13 @@ import csv
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+
 import pandas as pd
 import IPython
 get_ipython().run_line_magic('matplotlib', 'inline')
 IPython.display.set_matplotlib_formats('svg')
 pd.plotting.register_matplotlib_converters()
-
+sns.set_style("whitegrid")
 
 import pyspark
 from pyspark.sql import SparkSession
@@ -24,47 +25,16 @@ spark = SparkSession(sc)
 spark
 from pyspark.sql.types import DateType, IntegerType, MapType, StringType, FloatType
 
-#get keyfood_sample_items dataframe with valid upc code (only number after - )
-keyfood_sample_items = spark.read.load('keyfood_sample_items.csv', format='csv', header=True, inferSchema=True)
-keyfood_sample_items = keyfood_sample_items.withColumn('UPC code', (F.split(keyfood_sample_items['UPC code'], "-"))[1])
-
-
-# get keyfood_products datafrome with vliad output for upc and price columns
-keyfood_products = spark.read.load('keyfood_products.csv', format='csv', header=True, inferSchema=True)
-keyfood_products = keyfood_products.withColumn('upc', (F.split(keyfood_products['upc'], "-"))[1])
-
-def get_price(price):
-  i = 4
-  while i < len(price) and (price[i] in ['1','2','3','4','5','6','6','8', '9','0','.']):
-    i += 1
-  return float(price[1:i])
-
-udfExpand = F.udf(get_price, FloatType())
-keyfood_products_new = keyfood_products.select('store', 'department', 'upc', 'product',
-                 udfExpand(keyfood_products['price']).alias('Price($)'))
-#keyfood_products_new.show()
-
-# get keyfood_nyc_stores dataframe wih valid output for Food Insecurity column
-keyfood_nyc_stores = pd.read_json('keyfood_nyc_stores.json')
-keyfood_nyc_stores2 = keyfood_nyc_stores.iloc[[1,29,30]]
-keyfood_nyc_stores3 = keyfood_nyc_stores2.T
-keyfood_nyc_stores3.reset_index(drop=True, inplace=True)
-keyfood_nyc_stores_new = spark.createDataFrame(keyfood_nyc_stores3)
-
-
-def get_Insecurity(insecurity):
-  return insecurity*100
-udfExpand = F.udf(get_Insecurity, FloatType())
-keyfood_nyc_stores_new = keyfood_nyc_stores_new.select('name', 'communityDistrict',
-                 udfExpand(keyfood_nyc_stores_new['foodInsecurity']).alias('Food Insecurity'))
-
-#keyfood_nyc_stores_new.show()
-
-#join tables and return dataframe with valid coumns (Item Name, Price($) and Food Insecurity)
-dfTask1 = keyfood_sample_items.join(keyfood_products_new, keyfood_sample_items['UPC code'] == keyfood_products_new['upc'], how='inner')
-dfTask2 = dfTask1.join(keyfood_nyc_stores_new, dfTask1.store == keyfood_nyc_stores_new.name, how = 'left' )
-outputTask1 = dfTask2.select('Item Name', 'Price($)', 'Food Insecurity')
-
+keyfood_nyc_stores = json.load(open('keyfood_nyc_stores.json','r'))
+Sample_items = dict(map(lambda x: (x[0].split('-')[-1], x[1]), pd.read_csv('keyfood_sample_items.csv').to_numpy()))
+udfGetName = F.udf(lambda x: Sample_items.get(x.split('-')[-1],None), T.StringType())
+udfGetPrice = F.udf(lambda x: float(x.split()[0].lstrip('$')), T.FloatType())
+udfGetScore = F.udf(lambda x: keyfood_nyc_stores[x]['foodInsecurity']*100, T.FloatType())
+from re import escape
+outputTask1 = spark.read.csv('keyfood_products.csv',
+                         header = True, escape = '"')\
+                         .select(udfGetName('upc').alias('name'), udfGetPrice('price'), udfGetScore('store'))\
+                         .dropna(subset = ['name'])
 
 outputTask1.to_csv('input',sep='\t',index=False)
 
